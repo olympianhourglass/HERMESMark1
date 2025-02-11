44be1ab4e5d871024cd18467191b763e0e06ed99
Albert Yu <albertyu@Alberts-Laptop.local>
2025-02-10 19:34:57 -0700

    initial commit

---
+++ HERMESMark1/HERMESMark1App.swift
@@ -0,0 +1,17 @@
+//
+//  HERMESMark1App.swift
+//  HERMESMark1
+//
+//  Created by Albert Yu on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct HERMESMark1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HERMESMark1Tests/HERMESMark1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  HERMESMark1Tests.swift
+//  HERMESMark1Tests
+//
+//  Created by Albert Yu on 2/10/25.
+//
+
+import Testing
+@testable import HERMESMark1
+
+struct HERMESMark1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
